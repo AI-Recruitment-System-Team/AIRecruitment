8c35ea00bbbc4ced14df165dfa9255f95d36a20b
MAC <mac@MACs-MacBook-Air.local>
2026-07-20 23:41:37 +0300

    Add ApplicationUser entity and update ApplicationDbContext

--- src/AIRecruitment.DAL/Context/ApplicationDbContext.cs
+++ src/AIRecruitment.DAL/Context/ApplicationDbContext.cs
@@ -1,14 +1,23 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using AIRecruitment.Domain.Entities;
+using System.Security.Cryptography;
 
 namespace AIRecruitment.DAL.Context;
 
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
-    : base(options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
     {
-        
     }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+
+    public DbSet<ApplicationUser> ApplicationUser { get; set; }
 }
--- src/AIRecruitment.Domain/Entities/ApplicationUser.cs
+++ src/AIRecruitment.Domain/Entities/ApplicationUser.cs
@@ -1,8 +1,22 @@
-using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
 
 namespace AIRecruitment.Domain.Entities;
 
 public class ApplicationUser : IdentityUser
 {
-    
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string AvatarUrl { get; set; }
+
+    public bool IsActive { get; set; }
+
+    // Navigational Properties
+
+    public CandidateProfile CandidateProfile { get; set; }
+
+    //public List<Job> Jobs { get; set; }
+
+    //public List<Notification> Notifications { get; set; }
 }
