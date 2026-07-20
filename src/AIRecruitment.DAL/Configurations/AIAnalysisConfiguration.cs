e1ed6acace94ddeecf9e13cf833fe3d893741685
shahdgaballah <shahdgabllah@gmail.com>
2026-07-20 23:28:33 +0300

    add CandidateSkill, Application, AIAnalysis entities and their configs

---
+++ src/AIRecruitment.DAL/Configurations/AIAnalysisConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AIRecruitment.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AIRecruitment.DAL.Configurations
+{
+    public class AIAnalysisConfiguration : IEntityTypeConfiguration<AIAnalysis>
+    {
+        public void Configure(EntityTypeBuilder<AIAnalysis> builder)
+        {
+            //
+        }
+    }
+}
---
+++ src/AIRecruitment.Domain/Enums/CandidateSkillLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AIRecruitment.Domain.Enums
+{
+    public enum CandidateSkillLevel
+    {
+        Beginner,
+        Intermediate,
+        Advanced
+    }
+}
