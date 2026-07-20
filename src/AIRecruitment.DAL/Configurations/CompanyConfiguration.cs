8348938c02b00ada63e8c4bae99a38a73e285cd3
shahdgaballah <shahdgabllah@gmail.com>
2026-07-20 22:44:45 +0300

    add CandidateProfile, Company, Resume entities and their configs

---
+++ src/AIRecruitment.DAL/Configurations/CompanyConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using AIRecruitment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AIRecruitment.DAL.Configurations
+{
+    public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .HasMaxLength(100);
+
+            builder.Property(c => c.Description)
+                .HasMaxLength(3000);
+
+            builder.Property(c => c.WebsiteUrl)
+                .HasMaxLength(500);
+
+            builder.Property(c => c.LogoUrl)
+                .HasMaxLength(500);
+           
+        }
+    }
+}
--- src/AIRecruitment.Domain/Entities/ApplicationUser.cs
+++ src/AIRecruitment.Domain/Entities/ApplicationUser.cs
@@ -1,4 +1,5 @@
-using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
 namespace AIRecruitment.Domain.Entities;
 
 public class ApplicationUser : IdentityUser
